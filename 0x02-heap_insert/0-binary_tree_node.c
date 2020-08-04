bae60d608693a6c32704322c4b2479899abeaa58
salmenz <salmen.zooro@gmail.com>
2020-08-04 18:38:19 +0100

    add node in tree

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,19 +9,20 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new;
-new = malloc(sizeof(binary_tree_t));
-if (new == NULL)
-    return (NULL);
-new->left = NULL;
-new->right = NULL;
-if (parent == NULL)
-{
-    new->parent = NULL;
-    new->n = value;
-    return (new);
-}
-new->parent = parent;
-new->n = value;
-return (new);
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	if (parent == NULL)
+	{
+		new->parent = NULL;
+		new->n = value;
+		return (new);
+	}
+	new->parent = parent;
+	new->n = value;
+	return (new);
 }
