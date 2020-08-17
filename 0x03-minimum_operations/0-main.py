257df2d52dda8073ca1292fd49dd8059452c424e
salmenz <salmen.zooro@gmail.com>
2020-08-17 21:00:01 +0100

    add new line

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -8,5 +8,5 @@ minOperations = __import__('0-minoperations').minOperations
 n = 35
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
-n = -1
+n = 545455154545
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
