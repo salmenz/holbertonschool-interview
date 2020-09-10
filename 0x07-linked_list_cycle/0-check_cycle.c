4a3c46b1b814f28ae6e1214521876ad1c94f4577
«salmenz» <salmen.zooro@gmail.com>
2020-09-10 18:46:08 +0100

    check cycle in list

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a loop in the list.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+
+	fast = list->next;
+	while (fast != NULL && slow != NULL && fast->next != NULL)
+	{
+		if (fast == slow)
+			return 1;
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return 0;
+}
