98a18b439b51d472e555688b510758ed0d23d9b9
salmenz <salmen.zooro@gmail.com>
2021-02-18 23:59:34 +0100

    utf

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
 """
-UTF-8 Validator Module
+UTF-8 validation
 """
 
 
 def validUTF8(data):
-    num_bytes = 0
-    for integer in data:
-        bin = format(integer, '#010b')[-8:]
-        print (bin)
-        if num_bytes == 0:
-            for bit in bin:
-                if bit == "0":
+    """
+    fn that determines if a given data set represents a valid UTF-8 encoding.
+        Prototype: def validUTF8(data)
+        Return: True if data is a valid UTF-8 encoding, else return False
+        A character in UTF-8 can be 1 to 4 bytes long
+        The data set can contain multiple characters
+        The data will be represented by a list of integers
+        Each integer represents 1 byte of data,
+            therefore you only need to handle the 8 least
+            significant bits of each integer
+    """
+    s_byts = 0
+    for num in data:
+        repres = format(num, '#010b')[-8:]
+        if s_byts == 0:
+            for b in repres:
+                if b == '0':
                     break
-                num_bytes += 1
-
-            if num_bytes == 0:
+                s_byts += 1
+            if s_byts == 0:
                 continue
-
-            if num_bytes > 4 or num_bytes == 1:
+            if s_byts == 1 or s_byts > 4:
                 return False
         else:
-            if bin[0] != "1" or bin[1] != "0":
+            if not (repres[0] == '1' and repres[1] == '0'):
                 return False
-
-        num_bytes -= 1
-
-    return num_bytes == 0
+        s_byts = s_byts - 1
+    if s_byts == 0:
+        return True
+    return False
