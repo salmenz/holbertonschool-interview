891ca4a237dbc2c6268a8802ab0896d73aa89177
salmenz <salmen.zooro@gmail.com>
2021-03-18 23:56:55 +0100

    NQueens Solving

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -7,51 +7,82 @@ This is a program that solves the N queens problem.
 import sys
 
 
-def valid_position(solution, pos):
-    """ verif if the position is valid """
-    for queen in solution:
-        if queen[1] == pos[1]:
+def zeromat(n):
+    board = []
+    for x in range(n):
+        row = []
+        for y in range(n):
+            row.append(0)
+        board.append(row)
+    return(board)
+
+
+def showmat(board, n):
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(n):
+            if board[i][j] == 1:
+                b.append(i)
+                b.append(j)
+                a.append(b)
+    print(a)
+
+
+def safepos(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
             return False
-        if (queen[0] + queen[1]) == (pos[0] + pos[1]):
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if (board[i][j]):
             return False
-        if (queen[0] - queen[1]) == (pos[0] - pos[1]):
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < n:
+        if (board[i][j]):
             return False
+        i = i + 1
+        j = j - 1
     return True
 
 
-def solve_queens(row, n, solution):
-    """ find the solution from the root down """
-    if (row == n):
-        print(solution)
-    else:
-        for col in range(n):
-            pos = [row, col]
-            if valid_position(solution, pos):
-                solution.append(pos)
-                solve_queens(row + 1, n, solution)
-                solution.remove(pos)
+def nqueen(board, col, n):
+    if (col == n):
+        showmat(board, n)
+        return True
+    res = False
+    for i in range(n):
+        if (safepos(board, i, col, n)):
+            board[i][col] = 1
+            res = nqueen(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
 
 
-def main(n):
-    """ Main Function """
-    solution = []
-    solve_queens(0, n, solution)
+def nq(board, n):
+    if (nqueen(board, 0, n) is False):
+        print("Solution does not exist")
+        exit(1)
 
 
-if __name__ == '__main__':
-    """ get input """
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: nqueens N')
-        sys.exit(1)
+        print("Usage: nqueens N")
+        exit(1)
     try:
-        input = int(sys.argv[1])
+        if not type(int(sys.argv[1])) is int:
+            print("N must be a number")
+            exit(1)
     except BaseException:
-        print('N must be a number')
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
         sys.exit(1)
-    input = int(sys.argv[1])
-    if input < 4:
-        print('N must be at least 4')
-        sys.exit(1)
-
-    """ Calling the main function """
-    main(input)
+    board = zeromat(n)
+    nq(board, n)
