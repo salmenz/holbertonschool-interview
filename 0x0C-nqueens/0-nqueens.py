de55126614f7207f75eb4b300381d228a1fb75f8
salmenz <salmen.zooro@gmail.com>
2021-03-18 23:43:51 +0100

    NQueens Solving

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-The N queens puzzle is the challenge of placing N non-attacking queens 
-on an N×N chessboard.
-This is a program that solves the N queens problem.
+Script that solves the N queens problem based on the general Backtracking
+algorithm.
+This is:
+procedure bt(c) is
+    if reject(P, c) then return
+    if accept(P, c) then output(P, c)
+    s  first(P, c)
+    while s  NULL do
+        bt(s)
+        s  next(P, s)
 """
 import sys
 
 
-def valid_position(solution, pos):
-    """ verif if the position is valid """
+def valid_pos(solution, pos):
+    """
+    Function that verifies if the position is valid
+    """
     for queen in solution:
         if queen[1] == pos[1]:
             return False
@@ -20,37 +29,42 @@ def valid_position(solution, pos):
 
 
 def solve_queens(row, n, solution):
-    """ find the solution from the root down """
+    """
+    Function that finds the solution recursively, from the root down
+    """
     if (row == n):
         print(solution)
     else:
         for col in range(n):
             pos = [row, col]
-            if valid_position(solution, pos):
+            if valid_pos(solution, pos):
                 solution.append(pos)
                 solve_queens(row + 1, n, solution)
                 solution.remove(pos)
 
 
 def main(n):
-    """ Main Function """
+    """
+    Main function
+    """
     solution = []
+    """ From root(0) down(n) """
     solve_queens(0, n, solution)
 
 if __name__ == '__main__':
-    """ get input """
+    """ Validate the arguments from OS """
     if len(sys.argv) != 2:
         print('Usage: nqueens N')
         sys.exit(1)
     try:
-        input = int(sys.argv[1])
+        i = int(sys.argv[1])
     except BaseException:
         print('N must be a number')
         sys.exit(1)
-    input = int(sys.argv[1])
-    if input < 4:
+    i = int(sys.argv[1])
+    if i < 4:
         print('N must be at least 4')
         sys.exit(1)
 
     """ Calling the main function """
-    main(input)
+    main(i)
