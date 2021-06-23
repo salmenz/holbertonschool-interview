bc747d7d5b227abb3adbb0ec1dac86f9e5bfdca8
salmenz <salmen.zooro@gmail.com>
2021-06-24 00:09:42 +0100

    makeChange

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""determine the fewest number of coins needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed
+    to meet a given amount total"""
+    s_coins = sorted(coins, reverse=True)
+    s = 0
+    k = 0
+    i = 0
+    while s < total and i < len(coins):
+        if total - s >= s_coins[i]:
+            s += s_coins[i]
+            k += 1
+        else:
+            i += 1
+    return k if s == total else -1
